e7745640c13e3a13387ac241502ed89b1caf7155
duartrosar <74115478+duartrosar@users.noreply.github.com>
2022-09-02 18:14:51 +0100

    Init project

---
+++ VetManage/VetManage.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace VetManage.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
